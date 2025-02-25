d90c0d63e1480c15873938d33c4ee0ec3065f073
sakae .s <user@s3110MacBook-Air.local>
2025-02-25 19:16:00 +0900

    “（2/25ドロワー設定）”

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
-// ドロワーメニュー
-$(function(){
-    //メニューボタンをクリックしたら
-    $(".sp_menu").click(function(){
-    //「.menu」に「.active」を追加・削除
+'use script';
+
+$(function () {
+  //メニューボタンをクリックしたら
+  $(".sp_menu").click(function () {
+    //「.sp_menu」に「.active」を追加・削除
     $(this).toggleClass("active");
     //「.nav」に「.active」を追加・削除
     $(".drawer_nav").toggleClass("active");
-    });
-    });
+    // ナビゲーションメニュー内のリンク（.drawer_item a）がクリックされたときの処理を定義
+    $(".drawer_item a").click(function () {
+      //メニューボタンから「.active」クラスを削除
+      $(".sp_menu").removeClass("active")
+      // ナビゲーションメニューから「.active」クラスを削除
+      $(".drawer_nav").removeClass("active")
+    })
+  });
+});
