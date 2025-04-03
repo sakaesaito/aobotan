aacdff15213694752d49f49fce31dab20b77783c
sakae .s <user@s3110MacBook-Air.local>
2025-04-04 02:25:22 +0900

    “（4/3モーダル途中）”

--- main.js
+++ main.js
@@ -156,3 +156,17 @@ const swiper = new Swiper('.swiper', {
     }
   }
 });
+
+// モーダル
+$(function () {
+  $('.js-open').click(function () {
+    $("body").addClass("no_scroll"); // 背景固定させるクラス付与
+    var id = $(this).data('id'); // 何番目のキャプション（モーダルウィンドウ）か認識
+    $('#overlay, .modal-window[data-id="modal' + id + '"]').fadeIn();
+  });
+  // オーバーレイクリックでもモーダルを閉じるように
+  $('.js-close , #overlay').click(function () {
+    $("body").removeClass("no_scroll"); // 背景固定させるクラス削除
+    $('#overlay, .modal-window').fadeOut();
+  });
+});
